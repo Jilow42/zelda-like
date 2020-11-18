ab8a3593bc62887fe4ed12663f7abf6b058ec571
Flo <florian-lina@outlook.fr>
2020-11-18 10:10:07 +0100

    (core): first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { } from 'react-redux';
+
+const App = () => (
+  <div>Hello world</div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
